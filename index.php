2a9544aa973441f6e780925e9fd691e846955a66
Niklas <faz@live.se>
2010-08-11 19:25:48 +0200

    Har skapat indexfilen och skrivit in en basic html 4 strict template för vidare utveckling

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+// PHP-kod ska vara här :)
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<title>Tubify</title>
+	</head>
+	<body>
+		<div id="wrapper">
+			<p>Hej världen</p>
+		</div>
+	</body>
+</html>
